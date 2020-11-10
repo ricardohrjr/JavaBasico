6d3b6e9dfdf8fd8d824de5d1a98ac87a14170e11
ricardohrjr <ricardohrjr@hotmail.com>
2020-11-09 22:21:47 -0300

    teste

---
+++ src/aulajava1/main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package aulajava1;
+
+/**
+ * @author Casa
+ *
+ */
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
